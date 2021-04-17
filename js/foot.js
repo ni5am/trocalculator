add1e2e505355b21eeee5107eb56bd7da8993744
Kutsuru <kutsuru@gmail.com>
2021-04-18 01:33:32 +0800

    fix: Fixed Chaos and Blind resistance wrongly computed
    Related to issue #399

--- js/foot.js
+++ js/foot.js
@@ -5754,10 +5754,10 @@ function KakutyouKansuu(){
 		JyoutaiTaisei[1] = JyoutaiTaisei[0];
 		JyoutaiTaisei[2] = n_A_MDEF;
 		JyoutaiTaisei[3] = n_A_LUK;
-		JyoutaiTaisei[4] = (n_A_INT + n_A_VIT) * 50;
+		JyoutaiTaisei[4] = (n_A_INT + n_A_VIT) * 0.5;
 		JyoutaiTaisei[5] = n_A_INT;
 		JyoutaiTaisei[6] = JyoutaiTaisei[0];
-		JyoutaiTaisei[7] = (n_A_STR + n_A_INT) * 50;
+		JyoutaiTaisei[7] = (n_A_STR + n_A_INT) * 0.5;
 		JyoutaiTaisei[8] = JyoutaiTaisei[0];
 		JyoutaiTaisei[9] = JyoutaiTaisei[2];
 		for(var i=0;i<=9;i++){
